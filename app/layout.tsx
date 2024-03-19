732ca81b09427a18f251f2c229d8c675e23f97c0
huanghsheng <289436567@qq.com>
2024-03-19 10:11:21 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://nextjs-dashboard-delta-plum-26.vercel.app/'),
+}
 export default function RootLayout({
   children,
 }: {
